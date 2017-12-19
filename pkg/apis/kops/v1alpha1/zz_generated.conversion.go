4c4855e7bcb44a82cd41c348f2110cb22135de50
Valdis Rigdon <valdis.rigdon@appian.com>
2017-12-19 12:45:18 -0500

    fixup! WIP: profiling flag

--- pkg/apis/kops/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha1/zz_generated.conversion.go
@@ -2054,6 +2054,9 @@ func autoConvert_v1alpha1_KubeSchedulerConfig_To_kops_KubeSchedulerConfig(in *Ku
 	out.Master = in.Master
 	out.LogLevel = in.LogLevel
 	out.Image = in.Image
+	if err := v1.Convert_bool_To_Pointer_bool(&in.Profiling, &out.Profiling, s); err != nil {
+		return err
+	}
 	if in.LeaderElection != nil {
 		in, out := &in.LeaderElection, &out.LeaderElection
 		*out = new(kops.LeaderElectionConfiguration)
@@ -2077,6 +2080,9 @@ func autoConvert_kops_KubeSchedulerConfig_To_v1alpha1_KubeSchedulerConfig(in *ko
 	out.Master = in.Master
 	out.LogLevel = in.LogLevel
 	out.Image = in.Image
+	if err := v1.Convert_Pointer_bool_To_bool(&in.Profiling, &out.Profiling, s); err != nil {
+		return err
+	}
 	if in.LeaderElection != nil {
 		in, out := &in.LeaderElection, &out.LeaderElection
 		*out = new(LeaderElectionConfiguration)
--- pkg/apis/kops/v1alpha2/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha2/zz_generated.conversion.go
@@ -2316,6 +2316,9 @@ func autoConvert_v1alpha2_KubeSchedulerConfig_To_kops_KubeSchedulerConfig(in *Ku
 	out.Master = in.Master
 	out.LogLevel = in.LogLevel
 	out.Image = in.Image
+	if err := v1.Convert_bool_To_Pointer_bool(&in.Profiling, &out.Profiling, s); err != nil {
+		return err
+	}
 	if in.LeaderElection != nil {
 		in, out := &in.LeaderElection, &out.LeaderElection
 		*out = new(kops.LeaderElectionConfiguration)
@@ -2339,6 +2342,9 @@ func autoConvert_kops_KubeSchedulerConfig_To_v1alpha2_KubeSchedulerConfig(in *ko
 	out.Master = in.Master
 	out.LogLevel = in.LogLevel
 	out.Image = in.Image
+	if err := v1.Convert_Pointer_bool_To_bool(&in.Profiling, &out.Profiling, s); err != nil {
+		return err
+	}
 	if in.LeaderElection != nil {
 		in, out := &in.LeaderElection, &out.LeaderElection
 		*out = new(LeaderElectionConfiguration)
